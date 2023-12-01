97b63be079486e568667cf729eb70d93b7f40af7
Ricardo Larrahondo <ricardolarrahondo2@hotmail.com>
2023-12-01 16:57:11 +0100

    solve day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val digitsAsString = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    fun getDigitFromLine(line: String, last: Boolean, includeStringDigits: Boolean): Int {
+        for ((index, c) in if (last) line.withIndex().reversed() else line.withIndex()) {
+            if (c.isDigit()) return c.digitToInt()
+            else {
+                if (includeStringDigits) {
+                    for ((n, s) in digitsAsString.withIndex()) {
+                        val startIndex = if (last) index - s.length + 1 else index
+                        val endIndex = if (last) index + 1 else index + s.length
+                        if (line.safeSubstring(startIndex, endIndex) == s) return n + 1
+                    }
+                }
+            }
+        }
+        throw IllegalStateException("No digit found in line: $line")
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun solve(input: List<String>, includeStringDigits: Boolean): Int {
+        return input.sumOf {
+            val first = getDigitFromLine(it, false, includeStringDigits)
+            val last = getDigitFromLine(it, true, includeStringDigits)
+            "${first}${last}".toInt()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val testInput = readInput("Day01_test", "day1")
+    check(solve(testInput, false) == 142)
+    val part2TestInput = readInput("Day01_test_part2", "day1")
+    check(solve(part2TestInput, true) == 281)
+
+    val input = readInput("Day01", "day1")
+    solve(input, false).println()
+    solve(input, true).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String,day:String) = Path("src/data/$day/$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
@@ -19,3 +19,8 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun String.safeSubstring(startIndex: Int, endIndex: Int): String {
+    if (startIndex < 0 || endIndex > this.length) return ""
+    return this.substring(startIndex, endIndex)
+}
